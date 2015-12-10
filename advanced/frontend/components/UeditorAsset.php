063ed31850bcedcb5d61c8e67c8867847f221aa7
abwxwx <abwxwx@126.com>
2015-12-10 19:27:37 +0800

    20151210
    更改编辑器的上传图片位置
    修改yii的资源发布位置

--- advanced/frontend/components/UeditorAsset.php
+++ advanced/frontend/components/UeditorAsset.php
@@ -1,6 +1,6 @@
 <?php
 namespace frontend\components;
-
+use Yii;
 use yii\web\AssetBundle;
 
 class UeditorAsset extends AssetBundle
@@ -13,7 +13,8 @@ class UeditorAsset extends AssetBundle
     ];
     public function init()
     {
-        $this->sourcePath =$_SERVER['DOCUMENT_ROOT'].\Yii::getAlias('@web').'/ueditor'; //设置资源所处的目录
-        //echo $this->sourcePath; die;
+        $this->basePath = $_SERVER['DOCUMENT_ROOT'].\Yii::getAlias('@web').'/ueditor'; //设置资源所处的目录
+        $this->baseUrl = Yii::getAlias('@web').'/ueditor';
+        //echo $this->baseUrl; die;
     }
 }
